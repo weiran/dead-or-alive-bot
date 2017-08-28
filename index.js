10f3decfb616036b786801baf7f77a6910db1951
Eddie Lee <eddielee6@me.com>
2017-08-28 18:58:18 +0100

    Never forget, Harambe. May 28, 2016. RIP. 😢

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const DeadOrAliveService = require('./DeadOrAliveService');
 
 const bot = new Telegraf(process.env.BOT_TOKEN_DEV);
 
+const customResponses = {
+    harambe: 'Harambe, our lord and saviour was gunned down at 4pm on May 28, 2016. RIP.'
+};
+
 bot.on('text', async (context) => {
     const message = context.message;
     let searchTerm = message.text;
@@ -21,20 +25,25 @@ bot.on('text', async (context) => {
         }
     }
 
-    try {
-        const deadOrAliveService = new DeadOrAliveService();
-        const result = await deadOrAliveService.search(searchTerm);
-        let response = null;
-        if (result && result.isDead) { // dead
-            response = `[${result.name}](${result.wikipediaUrl}) died aged ${result.age} on ${result.dateOfDeath}.`;
-        } else if (result && !result.Dead) { // alive
-            response = `[${result.name}](${result.wikipediaUrl}) is alive and kicking at ${result.age} years old.`;
-        } else { // not found
-            response = `Couldn't find a person named ${searchTerm}.`;
+    const customResponse = customResponses[searchTerm.toLowerCase()];
+    if (customResponse) {
+        context.replyWithMarkdown(customResponse);
+    } else {
+        try {
+            const deadOrAliveService = new DeadOrAliveService();
+            const result = await deadOrAliveService.search(searchTerm);
+            let response = null;
+            if (result && result.isDead) { // dead
+                response = `[${result.name}](${result.wikipediaUrl}) died aged ${result.age} on ${result.dateOfDeath}.`;
+            } else if (result && !result.Dead) { // alive
+                response = `[${result.name}](${result.wikipediaUrl}) is alive and kicking at ${result.age} years old.`;
+            } else { // not found
+                response = `Couldn't find a person named ${searchTerm}.`;
+            }
+            context.replyWithMarkdown(response);
+        } catch (e) {
+            context.reply('Oops, something went wrong.');
         }
-        context.replyWithMarkdown(response);
-    } catch (e) {
-        context.reply('Oops, something went wrong.');
     }
 });
 
