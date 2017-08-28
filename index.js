1a6abca9c7aa4524b770759583796776abe70ce5
Weiran Zhang <weiran@zhang.me.uk>
2017-08-28 11:21:40 +0100

    Added support for commands in groups

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const bot = new Telegraf(process.env.TELEGRAM_TOKEN || config.TELEGRAM_TOKEN);
 const DeadOrAliveService = require("./DeadOrAliveService");
 
 bot.on("text", context => {
-    const searchTerm = context.message.text;
+    let message = context.message;
+    let searchTerm = message.text;
+    if (message.entities !== undefined && message.entities.length > 0) {
+        let commandOffset = message.entities[0];
+        searchTerm = searchTerm.substring(commandOffset.offset + commandOffset.length, searchTerm.length - 1);
+    }
     const deadOrAliveService = new DeadOrAliveService();
     deadOrAliveService.search(searchTerm)
     .then(result => {
