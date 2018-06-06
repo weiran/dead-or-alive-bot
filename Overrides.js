2b81e8a47e1a1f7601c19baca257e12babaeced3
Weiran Zhang <weiran@zhang.me.uk>
2018-06-06 20:03:10 +0100

    Fixed dates of overrides
    Who knew Javascript dates, months start at 0.

--- Overrides.js
+++ Overrides.js
@@ -1,14 +1,14 @@
 module.exports = [{
     overrideSearchTerm: 'harambe',
     name: 'Harambe the gorilla',
-    dateOfBirth: new Date(1999, 5, 27),
-    dateOfDeath: new Date(2016, 5, 28),
+    dateOfBirth: new Date(1999, 4, 27),
+    dateOfDeath: new Date(2016, 4, 28),
     wikipediaUrl: 'https://en.wikipedia.org/wiki/Killing_of_Harambe'
 },
 {
     overrideSearchTerm: 'cecil',
     name: 'Cecil the lion',
-    dateOfBirth: new Date(2002, 1, 1),
-    dateOfDeath: new Date(2015, 7, 1),
+    dateOfBirth: new Date(2002, 0, 1),
+    dateOfDeath: new Date(2015, 6, 1),
     wikipediaUrl: 'https://en.wikipedia.org/wiki/Killing_of_Cecil_the_lion'
 }];
